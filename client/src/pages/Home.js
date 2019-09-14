9ba31edf3ffbc57f48608da68be4843be4a6681f
Pierce Keane <Pierce.Keane@gmail.com>
2019-09-14 10:52:06 -0400

    random push

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Container,Row,Col} from "../components/Grid"
 import Navbar from "../components/Navbar"
+import "./home.css"
 
 
 function Home() {
@@ -9,8 +10,8 @@ function Home() {
         <Navbar>Sub-Split!</Navbar>
         <Container>
             <Row>
-                <Col size ="md-6">hi</Col>
-                <Col size="md-6">hi</Col>
+                <Col size ="md-12 info"  ><h3 className = "mt-5">Welcome to Sub-spilt. Were you can manage your prescriptions and share them with friends!.</h3></Col>
+                
             </Row>
         </Container>
 
